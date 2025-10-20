04fbd8bc87045e4290013b7f187cb1c496e6fe74
Ong Min Xian <ongminxian999@gmail.com>
2025-10-20 20:49:50 +0800

    update color formula of profit

--- poe-profit-app/src/app/bosses/bosses.ts
+++ poe-profit-app/src/app/bosses/bosses.ts
@@ -65,13 +65,13 @@ export class BossesComponent implements OnInit {
     if (value > 0) {
       const max = Math.max(...this.bosses.map(b => Math.abs(b.profitInChaos)));
       const postiveNormalizationConst = Math.min(1, Math.abs(value) / max)
-      const positiveSat = 30 + postiveNormalizationConst * 70;
-      return `hsl(147, ${positiveSat}%, 50%)`;
+      const positiveSat = 20 + postiveNormalizationConst * 50;
+      return `hsl(147, ${positiveSat}%, 65%)`;
     } else {
       const min = Math.min(...this.bosses.map(b => Math.abs(b.profitInChaos)));
       const negativeNormalizationConst = Math.min(1, Math.abs(value) / min);
-      const negativeSat = 30 + negativeNormalizationConst * 70;
-      return `hsl(0, ${negativeSat}%, 50%)`;
+      const negativeSat = 20 + negativeNormalizationConst * 50;
+      return `hsl(0, ${negativeSat}%, 65%)`;
     }
   }
 }
